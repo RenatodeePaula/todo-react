e6a2a948f685270bd68b6b6f699997ccd2bb3a41
Renato de Paula~ <renatodepaula.tec@gmail.com>
2024-05-09 17:49:03 -0300

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+import Todo from "./components/Todo";
+
+import Patei from './components/Patei';
+
 import "./App.css";
 
 
 function App() {
   const [todos, setTodos] = useState([
-
     {
       id: 1,
       text: "Criar funcionalidade X no sistema",
@@ -23,24 +27,19 @@ function App() {
   ]);
 
   return (
-    <div className='app'>
-          <h1>Lista de Tarefas</h1>
-          <div className="todo-list">
-            {todos.map((todos) => (
-              <div className="todo">
-                <div className="content">
-                  <p> {todos.text} </p>
-                  <p className='category'>({todos.category})</p>
-                </div>
-                <div>
-                  <button>Completar</button>
-                  <button>X</button>
-                </div>
-              </div>              
-            ))}
-          </div>
-        </div>
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) =>(
+         <Todo todo={todo} />
+        ))}
+      </div>
+    </div>
   )
 }
+
+return (
+  <Patei pateeei={Patei} />
+)
 
 export default App
