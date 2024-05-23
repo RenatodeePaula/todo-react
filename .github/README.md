72e4bc41d68e7fe0dffac5e38235858f79b57887
Renato de Paula~ <renatodepaula.tec@gmail.com>
2024-05-23 17:13:26 -0300

    final do projeto

---
+++ .github/README.md
@@ -0,0 +1,25 @@
+<h1 align="center">Quiz Interativo</h1>
+
+<p align="center">
+#Projeto feito para estudo de JavaScript puro, um quiz com perguntas e respostas interativas.<br/>
+</p>
+
+<p align="center">
+|  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+<br>
+
+<p align="center">
+  <img src="preview.jpg" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- Git e Github
+- Vanilla JavaScript
+---
+
+Feito by Renato de Paula pelo curso Hora de Codar 
--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,12 @@ import Filter from './components/Filter';
 
 function App() {
   const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "Criar funcionalidade X no sistema",
-      category: "Trabalho",
-      isCompleted: false,
-    },
-    { id: 2,
-      text: "Ir para a academia", 
-      category: "Pessoal", 
-      isCompleted: false },
-    {
-      id: 3,
-      text: "Estudar React",
-      category: "Estudos",
-      isCompleted: false,
-    },
+ 
   ]);
   
   const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
 
   const addTodo = (text, category) => {
     const newTodos = [...todos, 
@@ -58,12 +45,23 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch}/>
-      <Filter/>
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
       <div className="todo-list">
         {todos
+        .filter((todo)=> filter === "All" 
+        ? true 
+        : filter === "Completed" 
+        ? todo.isCompleted 
+        : !todo.isCompleted 
+      ) 
         .filter((todo) => 
           todo.text.toLocaleLowerCase().includes(search.toLocaleLowerCase())
-        )
+        ) 
+        .sort((a, b) => 
+        sort ==="Asc" 
+          ? a.text.localeCompare(b.text)
+          : b.text.localeCompare(a.text)
+      )
         .map((todo) =>(
          <Todo 
           key={todo.id} 
